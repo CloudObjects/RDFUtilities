fad7e832a225b89828468f7869d4412f90caddb5
Lukas Rosenstock <lr@lukasrosenstock.net>
2019-08-13 15:33:11 +0200

    Support language-tagged strings

--- Arc2JsonLdConverter.php
+++ Arc2JsonLdConverter.php
@@ -7,95 +7,124 @@
 namespace CloudObjects\Utilities\RDF;
 
 use ML\IRI\IRI;
-use ML\JsonLD\JsonLD, ML\JsonLD\Quad, ML\JsonLD\TypedValue, ML\JsonLD\RdfConstants;
+use ML\JsonLD\JsonLD, ML\JsonLD\Quad, ML\JsonLD\LanguageTaggedString,
+    ML\JsonLD\TypedValue, ML\JsonLD\RdfConstants;
 
 class Arc2JsonLdConverter {
 
-  /**
-   * Converts an array of ARC2 triples into an array of RDF quads
-   * in JsonLD library format.
-   * @param array $triples ARC2 triples
-   */
-  public static function triplesToQuads(array $triples) {
-    $quads = array();
-    foreach ($triples as $t) {
-      $quads[] = new Quad(new IRI($t['s']), new IRI($t['p']),
-      ($t['o_type'] == 'uri') ? new IRI($t['o']) : new TypedValue($t['o'],
-      (isset($t['o_datatype']) && $t['o_datatype']!='') ? $t['o_datatype'] : RdfConstants::XSD_STRING));
+    /**
+     * Indicates whether the object is an instance of a literal type;
+     * either TypedValue or LanguageTaggedString.
+     * @param $object
+     * @return boolean
+     */
+    public static function isLiteral($object) {
+        return (is_a($object, TypedValue::class) || is_a($object, LanguageTaggedString::class));
     }
 
-    return $quads;
-  }
+    /**
+     * Converts an array of ARC2 triples into an array of RDF quads
+     * in JsonLD library format.
+     * @param array $triples ARC2 triples
+     */
+    public static function triplesToQuads(array $triples) {
+        $quads = array();
+        foreach ($triples as $t) {
+            if ($t['o_type'] == 'literal' && isset($t['o_lang']) && $t['o_lang'] != '')
+                $value = new LanguageTaggedString($t['o'], $t['o_lang']);
+            elseif ($t['o_type'] == 'literal')
+                $value = new TypedValue($t['o'],
+                    (isset($t['o_datatype']) && $t['o_datatype'] != '')
+                        ? $t['o_datatype'] : RdfConstants::XSD_STRING);
+            else
+                $value = new IRI($t[0]);
 
-  /**
-   * Converts an ARC2 index into an array of RDF quads in JsonLD
-   * library format.
-   * @param array $index ARC2 index
-   */
-  public static function indexToQuads(array $index) {
-    $quads = array();
-    foreach ($index as $subject => $predicates) {
-      foreach ($predicates as $predicate => $objects) {
-        foreach ($objects as $object) {
-          $quads[] = new Quad(new IRI($subject),
-            new IRI($predicate),
-            ($object['type']!='literal')
-              ? new IRI($object['value']) : new TypedValue($object['value'],
-            (isset($object['datatype']) && $object['datatype']!='')
-              ? $object['datatype'] : RdfConstants::XSD_STRING));
+            $quads[] = new Quad(new IRI($t['s']),
+                new IRI($t['p']),
+                $value);
         }
-      }
+
+        return $quads;
     }
-    return $quads;
-  }
 
-  /**
-   * Converts an array of RDF quads in JsonLD library format into
-   * an array of ARC2 triples.
-   * @param array $quads JsonLD quads
-   */
-  public static function quadsToTriples(array $quads) {
-    $arcTriples = array();
-    foreach ($quads as $q) {
-      $arcTriples[] = array(
-        's' => (string)$q->getSubject(),
-        'p' => (string)$q->getProperty(),
-        'o' => (is_a($q->getObject(), 'ML\JsonLD\TypedValue'))
-          ? $q->getObject()->getValue()
-          : (string)$q->getObject(),
-        'o_type' => (is_a($q->getObject(), 'ML\JsonLD\TypedValue'))
-          ? 'literal'
-          : (($q->getObject()->getScheme() == '_')
-            ? 'bnode'
-            : 'uri')
-      );
+    /**
+     * Converts an ARC2 index into an array of RDF quads in JsonLD
+     * library format.
+     * @param array $index ARC2 index
+     */
+    public static function indexToQuads(array $index) {
+        $quads = array();
+        foreach ($index as $subject => $predicates) {
+            foreach ($predicates as $predicate => $objects) {
+                foreach ($objects as $object) {
+                    if ($object['type'] == 'literal' && isset($object['lang']))
+                        $value = new LanguageTaggedString($object['value'], $object['lang']);
+                    elseif ($object['type'] == 'literal')
+                        $value = new TypedValue($object['value'],
+                        (isset($object['datatype']) && $object['datatype']!='')
+                          ? $object['datatype'] : RdfConstants::XSD_STRING);
+                    else
+                        $value = new IRI($object['value']);
+                    
+                    $quads[] = new Quad(new IRI($subject), new IRI($predicate), $value);
+                }
+            }
+        }
+        return $quads;
     }
-    return $arcTriples;
-  }
 
-  /**
-   * Converts an array of RDF quads in JsonLD library format into
-   * an ARC2 index.
-   * @param array $quads JsonLD quads
-   */
-  public static function quadsToIndex(array $quads) {
-    return \ARC2::getSimpleIndex(self::quadsToTriples($quads), 0);
-  }
+    /**
+     * Converts an array of RDF quads in JsonLD library format into
+     * an array of ARC2 triples.
+     * @param array $quads JsonLD quads
+     */
+    public static function quadsToTriples(array $quads) {
+        $arcTriples = [];
+        foreach ($quads as $q) {
+            $arcTriples[] = [
+                's' => (string)$q->getSubject(),
+                'p' => (string)$q->getProperty(),
+                'o' => self::isLiteral($q->getObject())
+                    ? $q->getObject()->getValue()
+                    : (string)$q->getObject(),
+                'o_type' => self::isLiteral($q->getObject())
+                    ? 'literal'
+                    : (($q->getObject()->getScheme() == '_')
+                        ? 'bnode'
+                        : 'uri'),
+                'o_lang' => is_a($q->getObject(), LanguageTaggedString::class) 
+                    ? $q->getObject()->getLanguage()
+                    : ''
+            ];
+        }
+        return $arcTriples;
+    }
 
-  /**
-   * Converts any JsonLD into an array of ARC2 triples.
-   * @param string|array|object $jsonLd JsonLD content
-   */
-  public static function jsonLdToTriples($jsonLd) {
-    return self::quadsToTriples(JsonLD::toRdf($jsonLd));
-  }
+    /**
+     * Converts an array of RDF quads in JsonLD library format into
+     * an ARC2 index.
+     * @param array $quads JsonLD quads
+     */
+    public static function quadsToIndex(array $quads) {
+        return \ARC2::getSimpleIndex(self::quadsToTriples($quads), 0);
+    }
 
-  /**
-   * Converts any JsonLD into an ARC2 index.
-   * @param string|array|object $jsonLd JsonLD content
-   */
-  public static function jsonLdToIndex($jsonLd) {
-    return self::quadsToIndex(JsonLD::toRdf($jsonLd));
-  }
+    /**
+     * Converts any JsonLD into an array of ARC2 triples.
+     * @param string|array|object $jsonLd JsonLD content
+     */
+    public static function jsonLdToTriples($jsonLd) {
+        if (is_array($jsonLd)) $jsonLd = (object)$jsonLd;
+        return self::quadsToTriples(JsonLD::toRdf($jsonLd));
+    }
+
+    /**
+     * Converts any JsonLD into an ARC2 index.
+     * @param string|array|object $jsonLd JsonLD content
+    */
+    public static function jsonLdToIndex($jsonLd) {
+        if (is_array($jsonLd)) $jsonLd = (object)$jsonLd;
+        return self::quadsToIndex(JsonLD::toRdf($jsonLd));
+    }
 
 }
--- tests/AllTests/ParserTest.php
+++ tests/AllTests/ParserTest.php
@@ -10,44 +10,61 @@ use ML\IRI\IRI;
 
 class ParserTest extends \PHPUnit_Framework_TestCase {
 
-  private function indexAssertions($output) {
-    $this->assertArrayHasKey('coid://example.com', $output);
-    $this->assertArrayHasKey('http://www.w3.org/2000/01/rdf-schema#label',
-      $output['coid://example.com']);
-    $this->assertEquals("Example",
-      $output['coid://example.com']['http://www.w3.org/2000/01/rdf-schema#label'][0]['value']);
-  }
+    private function indexAssertions($output) {
+        $this->assertArrayHasKey('coid://example.com', $output);
+        $this->assertArrayHasKey('http://www.w3.org/2000/01/rdf-schema#label',
+            $output['coid://example.com']);
+        $this->assertEquals("Example",
+            $output['coid://example.com']['http://www.w3.org/2000/01/rdf-schema#label'][0]['value']);
+        $this->assertArrayHasKey('http://www.w3.org/2000/01/rdf-schema#comment',
+            $output['coid://example.com']);
+        
+        $comments = $output['coid://example.com']['http://www.w3.org/2000/01/rdf-schema#comment'];
+        $this->assertCount(2, $comments);
+        $this->assertEquals("Localized Example", $comments[0]['value']);
+        $this->assertEquals("en", $comments[0]['lang']);
+        $this->assertEquals("Lokalisiertes Beispiel", $comments[1]['value']);
+        $this->assertEquals("de", $comments[1]['lang']);
+    }
 
-  public function testNTriplesToIndex() {
-    $input = "<coid://example.com> <http://www.w3.org/2000/01/rdf-schema#label> \"Example\" .";
-    $output = Parser::parseToIndex($input);
-    $this->indexAssertions($output);  
-  }
+    public function testNTriplesToIndex() {
+        $input = "<coid://example.com> <http://www.w3.org/2000/01/rdf-schema#label> \"Example\" .\n"
+            ."<coid://example.com> <http://www.w3.org/2000/01/rdf-schema#comment> \"Localized Example\"@en ."
+            ."<coid://example.com> <http://www.w3.org/2000/01/rdf-schema#comment> \"Lokalisiertes Beispiel\"@de .";
+        $output = Parser::parseToIndex($input);
+        $this->indexAssertions($output);  
+    }
 
-  public function testTurtleToIndex() {
-    $input = "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> . "
-      ."<coid://example.com> rdfs:label \"Example\" .";
-    $output = Parser::parseToIndex($input);
-    $this->indexAssertions($output);  
-  }
+    public function testTurtleToIndex() {
+        $input = "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> . "
+            ."<coid://example.com> rdfs:label \"Example\" ;"
+            ." rdfs:comment \"Localized Example\"@en ;"
+            ." rdfs:comment \"Lokalisiertes Beispiel\"@de .";
+        $output = Parser::parseToIndex($input);
+        $this->indexAssertions($output);  
+    }
 
-  public function testRDFXMLToIndex() {
-    $input = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-      ."<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" "
-      ."  xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\"> "
-      ." <rdf:Description rdf:about=\"coid://example.com\"> "
-      ."  <rdfs:label>Example</rdfs:label> "
-      ." </rdf:Description> "
-      ."</rdf:RDF>";
-    $output = Parser::parseToIndex($input);
-    $this->indexAssertions($output);  
-  }
+    public function testRDFXMLToIndex() {
+        $input = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+            ."<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" "
+            ."  xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\"> "
+            ." <rdf:Description rdf:about=\"coid://example.com\"> "
+            ."  <rdfs:label>Example</rdfs:label> "
+            ."  <rdfs:comment xml:lang=\"en\">Localized Example</rdfs:comment> "
+            ."  <rdfs:comment xml:lang=\"de\">Lokalisiertes Beispiel</rdfs:comment> "
+            ." </rdf:Description> "
+            ."</rdf:RDF>";
+        $output = Parser::parseToIndex($input);
+        $this->indexAssertions($output);  
+    }
 
-  public function testJsonLDToIndex() {
-    $input = "{ \"@id\" : \"coid://example.com\", "
-      ." \"http://www.w3.org/2000/01/rdf-schema#label\" : \"Example\" }";
-    $output = Parser::parseToIndex($input);
-    $this->indexAssertions($output);  
-  }
+    public function testJsonLDToIndex() {
+        $input = "{ \"@id\" : \"coid://example.com\", "
+            ." \"http://www.w3.org/2000/01/rdf-schema#label\" : \"Example\", "
+            ." \"http://www.w3.org/2000/01/rdf-schema#comment\" : [ { \"@value\" : \"Localized Example\", \"@language\" : \"en\" }, "
+            ." { \"@value\" : \"Lokalisiertes Beispiel\", \"@language\" : \"de\" } ] }";
+        $output = Parser::parseToIndex($input);
+        $this->indexAssertions($output);  
+    }
 
 }
